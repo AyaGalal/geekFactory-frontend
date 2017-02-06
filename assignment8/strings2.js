0790aa6fc7a6f5c691f7c88573fdd6ace8221ddb
ayagalal <ayaglal91@gmail.com>
2017-02-07 00:34:23 +0200

    fixing function :D

--- assignment8/strings2.js
+++ assignment8/strings2.js
@@ -4,6 +4,7 @@ exports.lengthOfLastWord = function(a){
        for(var i=str.length+1;i>=0;i--){
           if(str[i]){
             lastWord = str[i];
+            break;
           }
        }
        return lastWord.length;
